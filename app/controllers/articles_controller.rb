fa5d02159ad2b737362e6d40c8e6a38793314d84
Szymon Borucki <szymon.borucki@gmail.com>
2014-08-11 13:30:05 -0400

    Added search feature
    which doesn’t work and messed up the home page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,8 +4,15 @@ class ArticlesController < ApplicationController
   # GET /articles
   # GET /articles.json
   def index
-    @articles = Article.all
+    @search = Article.search do
+      fulltext params[:search]
+      with(:published_at).less_than(Time.zone.now)
+      facet(:publish_month)
+      with(:publish_month, params[:month]) if params[:month].present?
+    end
+    @articles = @search.results
   end
+
 
   # GET /articles/1
   # GET /articles/1.json
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :pictures
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
+  devise_for :users,  controllers: {omniauth_callbacks: "omniauth_callbacks"}
   # post '/articles/:id/undo', to: 'articles#undo', as: :undo
 
   # get '/articles/history', to: 'articles#history', as: :articles_history
